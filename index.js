0869c084da71f9cc0fd35fd8153fa5f4549885f1
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-04-06 15:13:21 -0700

    Dice game AY

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function randomImageSource() {
+  var randomNumber = Math.floor(Math.random()*6)+1;
+  var randomDiceImageFileName = "images/dice" + randomNumber + ".png";
+  return randomDiceImageFileName;
+}
+
+console.log(randomImageSource());
+
+
+
+// var randomNumber1 = Math.floor(Math.random()*6)+1;
+// var randomNumber2 = Math.floor(Math.random()*6)+1;
+//
+// var randomDiceImage = "dice" + randomNumber1 + ".png";
+// var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+//
+// var randomImageSource = "images/" + randomDiceImage;
+// var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource());
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource());
